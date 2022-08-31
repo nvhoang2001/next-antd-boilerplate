147a4137f0599b75343dfe8587a340a00de10111
Sotatek-HoangNgo <hoang.ngo@sotatek.com>
2022-08-31 10:12:03 +0700

    Build base

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, './')],
+    },
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.resolve(__dirname);
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
